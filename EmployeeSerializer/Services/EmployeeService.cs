c0c74b1ae0362347e1b7cec2c7b172ff22d217a9
Manthan Raj Rajoria <manthanraj.rajoria@metacube.com>
2025-01-13 14:47:13 +0530

    Added service and list page

---
+++ EmployeeSerializer/Services/EmployeeService.cs
@@ -0,0 +1,46 @@
+using System;
+using EmployeeSerializer.Models;
+
+namespace EmployeeSerializer.Services;
+
+public class EmployeeService
+{
+    private static List<Employee> employees = new List<Employee>();
+
+    public static List<Employee> GetEmployeeList(){
+        return employees;
+    }
+
+    public static bool AddEmployee(Employee newEmployee){
+        foreach(var employee in employees){
+            if(employee.Equals(newEmployee)){
+                return false;
+            }
+        }
+        employees.Add(newEmployee);
+        return true;
+    }
+
+    public static bool RemoveEmployee(Guid id){
+        Employee? employeeToRemove = null;
+        foreach(var employee in employees){
+            if(employee.Id == id){
+                employeeToRemove = employee;
+            }
+        }
+        if(employeeToRemove != null){
+            employees.Remove(employeeToRemove);
+            return true;
+        }
+        return false;
+    }
+
+    public static Employee? FindFriend(Guid id){
+        foreach(var employee in employees){
+            if(employee.Id == id){
+                return employee;
+            }
+        }
+        return null;
+    }
+}
